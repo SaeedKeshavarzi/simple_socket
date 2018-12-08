c7144a6026918531ba6bfbff2c12317aa9518f1f
Saeed Keshavarzi <Saeed@Keshavarzi.com>
2018-12-08 15:09:39 +0330

    allow unspecified client port

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include "pch.h"
 
 #include "io/sockio.h"
+#include "sync/resettable_event.h"
 
 #define PACKET_LEN (1500)
 
+manual_reset_event ready{ false };
+uint16_t server_port{ 0 };
+
 bool keep_recv{ true };
 bool keep_send{ true };
+
 void rx_server();
-void tx_client();
+void tx_client(uint16_t port);
 
 int main()
 {
@@ -16,8 +21,10 @@ int main()
 	printf("press enter to stop... \n");
 
 	std::thread server_thread{ rx_server };
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
-	std::thread client_thread{ tx_client };
+	ready.wait();
+
+	printf("server port: %d \n", server_port);
+	std::thread client_thread{ tx_client, server_port };
 
 	GET_CHAR();
 	keep_recv = false;
@@ -35,12 +42,15 @@ void rx_server()
 	char packet[PACKET_LEN];
 	int ret;
 
-	ret = tcp_server.create("192.168.0.10", 7121);
+	ret = tcp_server.create();
 	if (ret != 0)
 	{
 		printf("create server failed. error code: %d \n", ret);
 		return;
 	}
+
+	server_port = tcp_server.port();
+	ready.set();
 
 	ret = tcp_server.listen(socket);
 	if (ret != 0)
@@ -67,13 +77,13 @@ void rx_server()
 	tcp_server.close();
 }
 
-void tx_client()
+void tx_client(uint16_t port)
 {
 	socket_t tcp_client;
 	char packet[PACKET_LEN];
 	int ret;
 
-	ret = tcp_client.connect("192.168.0.10", 7171, "192.168.0.10", 7121, ip_protocol_t::tcp);
+	ret = tcp_client.connect("192.168.0.10", port, ip_protocol_t::tcp);
 	if (ret != 0)
 	{
 		printf("connect failed. error code: %d \n", ret);
